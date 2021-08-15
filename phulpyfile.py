916bdb2c2b61183ce8237bbc7548eba841d339ac
Jeferson Daniel <jeferson.daniel412@gmail.com>
2021-08-14 21:51:48 -0300

    Update phulpyfile.py

--- phulpyfile.py
+++ phulpyfile.py
@@ -26,7 +26,6 @@ def unit_test(phulpy):
         raise Exception('Unit tests failed')
     coverage_path = join(dirname(__file__), 'coverage.xml')
     xml = ET.parse(coverage_path).getroot()
-    unlink(coverage_path)
     if float(xml.get('line-rate')) < 1:
         raise Exception('Unit test is not fully covered')
 
