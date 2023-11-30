7ea504864fceb1fcaafba5a06687fa3875c6984b
Artucuno <artucunov@gmail.com>
2023-11-30 11:06:20 +1100

    Fix for OpenAPI (PydanticInvalidForJsonSchema) error (#48)

--- pydantic_mongo/fields.py
+++ pydantic_mongo/fields.py
@@ -1,10 +1,23 @@
 from typing import Any
 
 from bson import ObjectId
+from pydantic import GetJsonSchemaHandler
+from pydantic.json_schema import JsonSchemaValue
 from pydantic_core import core_schema
 
 
 class ObjectIdField(str):
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def __get_pydantic_json_schema__(
+            cls, _core_schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        return handler(core_schema.str_schema())
+
     @classmethod
     def __get_pydantic_core_schema__(
         cls, _source_type: Any, _handler: Any
@@ -18,7 +31,8 @@ class ObjectIdField(str):
         return core_schema.json_or_python_schema(
             json_schema=object_id_schema,
             python_schema=core_schema.union_schema(
-                [core_schema.is_instance_schema(ObjectId), object_id_schema]
+                [core_schema.no_info_plain_validator_function(cls.validate), core_schema.is_instance_schema(ObjectId),
+                 object_id_schema]
             ),
             serialization=core_schema.plain_serializer_function_ser_schema(
                 lambda x: str(x)
