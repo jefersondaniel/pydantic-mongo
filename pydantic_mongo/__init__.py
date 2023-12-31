4f8721b4c01458fe6c85424f14b1e14cea436ad7
Jylpah <jylpah@gmail.com>
2023-12-31 21:38:30 +0200

    Update __init__.py

--- pydantic_mongo/__init__.py
+++ pydantic_mongo/__init__.py
@@ -1,5 +1,5 @@
-from .abstract_repository import AbstractRepository 
-from .fields import ObjectIdField  
-from .version import __version__  
+from .abstract_repository import AbstractRepository as AbstractRepository 
+from .fields import ObjectIdField as ObjectIdField  
+from .version import __version__ as __version__  
 
 __all__ = [ "fields", "version", "abstract_repository" ]
