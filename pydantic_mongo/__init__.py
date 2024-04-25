11a2fb75a779506303b5b981ac7309a402d03482
Jeferson Daniel <jeferson.daniel412@gmail.com>
2024-04-24 23:58:11 -0300

    Fix black

--- pydantic_mongo/__init__.py
+++ pydantic_mongo/__init__.py
@@ -2,4 +2,9 @@ from .abstract_repository import AbstractRepository
 from .fields import ObjectIdAnnotation, ObjectIdField, PydanticObjectId
 from .version import __version__  # noqa: F401
 
-__all__ = ["AbstractRepository", "ObjectIdField", "ObjectIdAnnotation", "PydanticObjectId"]
+__all__ = [
+    "AbstractRepository",
+    "ObjectIdField",
+    "ObjectIdAnnotation",
+    "PydanticObjectId",
+]
